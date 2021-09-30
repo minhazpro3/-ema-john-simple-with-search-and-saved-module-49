51c56604c1c19ba89260344cc49fab2739d07a14
minhazpro3 <minhazfr8@gmail.com>
2021-09-30 22:08:09 +0600

    first commit

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h3>Github remote inventory. amar sonar bngla</h3>
+        </div>
+    );
+};
+
+export default Inventory;
